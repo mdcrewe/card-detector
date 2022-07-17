e4b0173ad323aa24ce51b26fe154ee32b1a299d3
mdcrewe <46567948+mdcrewe@users.noreply.github.com>
2022-07-17 15:43:03 -0230

    Create card_functions.py
    Started adding functions for adding templates and converting image to blurred, grayscaled

---
+++ card_functions.py
@@ -0,0 +1,116 @@
+############## Python-OpenCV Playing Card Detector Functions ###############
+#
+# Description: File containing all functions to be used in card detecting algorith
+
+import numpy as np
+import cv2
+import time
+
+### Constants ###
+
+# Adaptive threshold levels
+BKG_THRESH = 60
+CARD_THRESH = 30
+
+# Width and height of card corner, where rank and suit are
+CORNER_WIDTH = 32
+CORNER_HEIGHT = 84
+
+# Dimensions of rank train images
+RANK_WIDTH = 70
+RANK_HEIGHT = 125
+
+# Dimensions of suit train images
+SUIT_WIDTH = 70
+SUIT_HEIGHT = 100
+
+RANK_DIFF_MAX = 2000
+SUIT_DIFF_MAX = 700
+
+CARD_MAX_AREA = 120000
+CARD_MIN_AREA = 25000
+
+### Structures to hold query card and train card information ###
+
+class Query_card:
+    """Structure to store information about query cards in the camera image."""
+
+    def __init__(self):
+        self.contour = [] # Contour of card
+        self.width, self.height = 0, 0 # Width and height of card
+        self.corner_pts = [] # Corner points of card
+        self.center = [] # Center point of card
+        self.warp = [] # 200x300, flattened, grayed, blurred image
+        self.rank_img = [] # Thresholded, sized image of card's rank
+        self.suit_img = [] # Thresholded, sized image of card's suit
+        self.best_rank_match = "Unknown" # Best matched rank
+        self.best_suit_match = "Unknown" # Best matched suit
+        self.rank_diff = 0 # Difference between rank image and best matched train rank image
+        self.suit_diff = 0 # Difference between suit image and best matched train suit image
+
+class Train_ranks:
+    """Structure to store information about train rank images."""
+
+    def __init__(self):
+        self.img = [] # Thresholded, sized rank image loaded from hard drive
+        self.name = "Placeholder"
+
+class Train_suits:
+    """Structure to store information about train suit images."""
+
+    def __init__(self):
+        self.img = [] # Thresholded, sized suit image loaded from hard drive
+        self.name = "Placeholder"
+
+### Functions ###
+def load_ranks(filepath):
+    """Loads rank images from directory specified by filepath. Stores
+    them in a list of Train_ranks objects."""
+
+    train_ranks = []
+    i = 0
+    
+    for Rank in ['Ace','Two','Three','Four','Five','Six','Seven',
+                 'Eight','Nine','Ten','Jack','Queen','King']:
+        train_ranks.append(Train_ranks())
+        train_ranks[i].name = Rank
+        filename = Rank + '.jpg'
+        train_ranks[i].img = cv2.imread(filepath+filename, cv2.IMREAD_GRAYSCALE)
+        i = i + 1
+
+    return train_ranks
+
+def load_suits(filepath):
+    """Loads suit images from directory specified by filepath. Stores
+    them in a list of Train_suits objects."""
+
+    train_suits = []
+    i = 0
+    
+    for Suit in ['Spades','Diamonds','Clubs','Hearts']:
+        train_suits.append(Train_suits())
+        train_suits[i].name = Suit
+        filename = Suit + '.jpg'
+        train_suits[i].img = cv2.imread(filepath+filename, cv2.IMREAD_GRAYSCALE)
+        i = i + 1
+
+    return train_suits
+  
+def image_preprocess(img):
+  im_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)   # Convert image to grayscale
+  im_blur = cv2.medianBlur(im_grey,(5,5))          # Blur image to reduce effect of noise
+
+  # im_width = np.shape(im_blur)[0]
+  # im_height = np.shape(im_blur)[1]
+  bkg_level = im_blur[0][0]               # Finding intensity of corner pixel
+  thresh_level = bkg_level + BKG_THRESH   # Setting threshold value to some intensity > corner intensity
+  
+  ret,thresh = cv2.threshold(im_blur, thresh_level, 255, cv2.THRESH_BINARY)
+  
+  return thresh
+
+def contours(image):
+  
+  
+  
+  
