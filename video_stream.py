cc893d91d423ed5967f3b363c35dc127352dea62
mdcrewe <46567948+mdcrewe@users.noreply.github.com>
2022-07-17 14:29:05 -0230

    Create video_stream.py
    Created video streaming functions (heavily copied from Evan Juras) to allow access to frames from video stream and creation of video stream.

---
+++ video_stream.py
@@ -0,0 +1,33 @@
+from threading import Thread
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+class Stream:
+  def __init__(self,res,fr):
+    self.camera = PiCamera()
+    self.camera.resolution = res
+    self.camera.framerate = fr
+    self.rawCapture = PiRGBArray(self.camera,size = res)
+    self.stream = self.camera.capture_continuous(self.rawCapture, format = "bgr", use_video_port = True)
+    self.frame = []
+    self.stop = False
+    
+  def start(self):
+    Thread(target=self.update, args=()).start()
+    return self
+  
+  def update(self):
+    for f in self.stream:
+      self.frame = f.array
+      self.rawCapture.truncate(0)
+      if self.stop:
+        self.stream.close()
+        self.rawCapture.close()
+        self.camera.close()
+        
+  def read(self):
+    return self.frame
+  
+  def stop(self):
+    self.stopped = True
